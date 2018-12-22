bcdda6afa1d2e6ac88c5292dca361a97aef24d93
HagerSameh <hagersameh13@gmail.com>
2018-12-22 20:10:55 +0200

    SignIn

---
+++ DataBase.java
@@ -0,0 +1,17 @@
+package dawarly;
+
+import java.util.ArrayList;
+
+public class DataBase 
+{
+    public static ArrayList<User> userDB = new ArrayList(); 
+    public static ArrayList<Item> PersonalItemDB = new ArrayList();
+    public static ArrayList<Item> DeviceItemDB = new ArrayList();
+    public static ArrayList<Item> PapersItemDB = new ArrayList();
+    
+    public DataBase() 
+    {
+        
+    }
+    
+}
---
+++ Project.java
@@ -0,0 +1,26 @@
+
+package project; 
+
+import static project.DataBase.userDB;
+import static java.lang.System.*;
+import java.util.*;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+
+public class Project {
+
+    
+    
+    public static void main(String[] args) {
+      
+        
+     Menu menu = new Menu();
+        menu.show();
+    }
+    
+}
+
+
